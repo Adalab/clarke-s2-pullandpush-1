060b94225cdfe00b0cbf3554f5c7bdf3f222dcad
Adalaber <mvtercero85@gmail.com>
2017-12-08 14:20:20 +0100

    Add code js select month job and select month education

--- js/main.js
+++ js/main.js
@@ -1,3 +1,79 @@
 "use strict";
 
 /*JAVASCRIPT MARÍA*/
+
+//MESES JOB DESDE
+
+var monthOption = document.querySelector('#month-from-job');
+monthOption.addEventListener('change',showAlertWithMonthFrom);
+
+function showAlertWithMonthFrom(event) {
+  var select= event.currentTarget;
+  var input= select.options[select.selectedIndex];
+
+  alert ('El mes seleccionado desde es ' + input.value);
+}
+
+//MESES JOB HASTA
+
+var monthOption = document.querySelector('#month-to-job');
+monthOption.addEventListener('change',showAlertWithMonthTo);
+
+function showAlertWithMonthTo(event) {
+  var select= event.currentTarget;
+  var input= select.options[select.selectedIndex];
+
+  alert ('El mes seleccionado hasta es ' + input.value);
+}
+
+//MESES EDUCATION desde
+
+var monthOption = document.querySelector('#month-from-education');
+monthOption.addEventListener('change',showAlertWithMonthFrom);
+
+function showAlertWithMonthFrom(event) {
+  var select= event.currentTarget;
+  var input= select.options[select.selectedIndex];
+
+  alert ('El mes seleccionado en estudios desde es ' + input.value);
+}
+
+//MESES EDUCATION hasta
+
+var monthOption = document.querySelector('#month-to-education');
+monthOption.addEventListener('change',showAlertWithMonthTo);
+
+function showAlertWithMonthTo(event) {
+  var select= event.currentTarget;
+  var input= select.options[select.selectedIndex];
+
+  alert ('El mes seleccionado en estudios hasta es ' + input.value);
+}
+
+
+
+
+//AÑOS
+
+// var content = '';
+// var currentYear=2017;
+// for (var i = 0; i < array.length; i++) {
+// array[i]
+// }
+//
+// content += '<option>' + year + '</option>'
+// pruebaElem.innerHTML = content;
+//
+// --------------------------------------------------------------------------------
+//
+// function addYears(initYear, currentYear) {
+//   var acumulador= ''
+//   for (var i= initYear; i <= currentYear; i++) {
+//     acumulador= acumulador + '<option>'+ i + '</option>'
+//   }
+//   return acumulador;
+// }
+//
+// var selectorYears = document.querySelector ('#selectorYears');
+//
+// selectorYears.innerHTML + selectorYears.innerHTML + addYears(1950, 2017);
