70edd3eefc11355a2c25dab382c12a345336a9a5
Elena <elenacerezo83@gmail.com>
2017-12-11 16:37:51 +0100

    problems collapse-tabs solved

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
-
-//  con este evento se activa o desactiva el ojo
+ //
+ // con este evento se activa o desactiva el ojo
 var toggle = function(){
   document.querySelectorAll('.eye-logo').forEach(function(input){
     if (input.classList.contains('hidden'))
@@ -19,35 +19,35 @@ window.onload = function (){
   });
 };
 
-var showLegend = document.querySelectorAll('.form-tittle');
-var collapseTab = document.querySelectorAll('.tab');
-var idLegend = event.currentTarget.getAttribute('data-id');
-
-function showFieldset(event) {
-  for (var idLegend = 0; idLegend < showLegend.length; idLegend++){
-    if (collapseTab.classList.contains('hidden')){
-    collapseTab.classList.remove('hidden');
-  }  else{
-    collapseTab.classList.add ('hidden');
-  };
-}
-var showLegend= document.querySelectorAll('.form-tittle');
-var idLegend = event.currentTarget.getAttribute('data-id');
-for (var idLegend = 0; idLegend < showLegend.length; idLegend++) {
-  showLegend.addEventListener('click', showFieldset(event));
-};
-
-
-// function showContent(event) {
-//   var content = document.querySelectorAll('.content');
-//   var idButton = event.currentTarget.getAttribute('data-id');
-//   content[idButton].classList.toggle('hidden');
-// }
+// var showLegend = document.querySelectorAll('.form-tittle');
+// var collapseTab = document.querySelectorAll('.tab');
+// var id = event.currentTarget.getAttribute('data-id');
 //
+// function showFieldset(event) {
+//   // event.currentTarget();
+//   for (var id = 0; id < showLegend.length; id++){
+//   if (collapseTab.classList.contains('hidden')){
+//     collapseTab.classList.remove('hidden');
+//   }  else{
+//     collapseTab.classList.add ('hidden');
+//   };
+//   }
+//   var id = currentTarget.getAttribute('data-id');
+//   for (var id = 0; id < showLegend.length; id++) {
+//     showLegend[id].addEventListener('click', showFieldset);
+//   }
+
+  var fieldset = document.querySelectorAll('.form-title');
+function showFieldset(event) {
+  var idFieldset = event.currentTarget.getAttribute('data-id');
+  var tab = document.querySelectorAll('.tab');
+  tab[idFieldset].classList.toggle('hidden');
+}
+
 // var editButton = document.querySelectorAll('.edit-button');
-// for (var i = 0; i < editButton.length; i++) {
-//   editButton[i].addEventListener('click', showContent);
-// };
+for (var i = 0; i < fieldset.length; i++) {
+  fieldset[i].addEventListener('click', showFieldset);
+};
 
 //
 // window.onload = function (){
