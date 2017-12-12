e44220c03e2acbb67c73e4daf2bf6e33b17c1fbc
Adalaber <mvtercero85@gmail.com>
2017-12-12 20:51:34 +0100

    Add for month and for ages

--- js/main.js
+++ js/main.js
@@ -2,139 +2,35 @@
 
 /*JAVASCRIPT MARÍA*/
 
-//¿Por qué si todos los meses tienen class selector-month no me aplica la función call-back en todos?
 
-//MESES JOB DESDE
-
-var monthOption = document.querySelector('.selector-month');
-monthOption.addEventListener('change',showAlertWithMonth);
-
-function showAlertWithMonth(event) {
-  var select= event.currentTarget;
-  var input= select.options[select.selectedIndex];
-
-  alert ('El mes seleccionado es ' + input.value);
-}
-
-// //MESES JOB HASTA
-//
-// var monthOption = document.querySelector('#month-to-job');
-// monthOption.addEventListener('change',showAlertWithMonthTo);
-//
-// function showAlertWithMonthTo(event) {
-//   var select= event.currentTarget;
-//   var input= select.options[select.selectedIndex];
-//
-//   alert ('El mes seleccionado hasta es ' + input.value);
-// }
-//
-// //MESES EDUCATION desde
-//
-// var monthOption = document.querySelector('#month-from-education');
-// monthOption.addEventListener('change',showAlertWithMonthFrom);
-//
-// function showAlertWithMonthFrom(event) {
-//   var select= event.currentTarget;
-//   var input= select.options[select.selectedIndex];
-//
-//   alert ('El mes seleccionado en estudios desde es ' + input.value);
-// }
-//
-// //MESES EDUCATION hasta
-//
-// var monthOption = document.querySelector('#month-to-education');
-// monthOption.addEventListener('change',showAlertWithMonthTo);
-//
-// function showAlertWithMonthTo(event) {
-//   var select= event.currentTarget;
-//   var input= select.options[select.selectedIndex];
-//
-//   alert ('El mes seleccionado en estudios hasta es ' + input.value);
-// }
-
-
-//Misma duda de meses para años
-
-//AÑOS DESDE JOB
-
-var selectorYears = document.querySelector ('.selector-year-from');
+//MESES
+var monthOption= ['','enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
 var acumulador= '';
-var currentYear= 2017;
-var firstYear= 1950;
 
-selectorYears.innerHTML + selectorYears.innerHTML + chooseYearsFrom(1950, 2017);
-
-function chooseYearsFrom(firstYear, currentYear) {
-  for (var i= firstYear; i <= currentYear; i++) {
-    acumulador= acumulador + '<option>'+ i + '</option>'
+for (var i= 0; i <= monthOption.length; i++) {
+acumulador= acumulador + '<option>'+ monthOption[i] + '</option>';
   }
-  return acumulador;
+
+var selectorMonth= document.querySelectorAll('.selector-month');
+for (var i = 0; i < selectorMonth.length; i++){
+  selectorMonth[i].innerHTML= acumulador;
 }
 
-selectorYears.innerHTML= acumulador;
 
-//AÑOS HASTA JOB
+//AÑOS
 
-var selectorYears = document.querySelector ('.selector-year-to');
+var selectorYears = document.querySelectorAll('.selector-year');
 var acumulador= '';
-var currentYear= 2017;
-var firstYear= 1950;
+var currentYear= 1950;
+var firstYear= 2017;
 
-selectorYears.innerHTML + selectorYears.innerHTML + chooseYearsTo(1950, 2017);
 
-function chooseYearsTo(firstYear, currentYear) {
-  for (var i= firstYear; i <= currentYear; i++) {
-    acumulador= acumulador + '<option>'+ i + '</option>'
-  }
-  return acumulador;
+for (var i= firstYear; i >= currentYear; i--) {
+  acumulador= acumulador + '<option>'+ i + '</option>'
 }
 
-selectorYears.innerHTML= acumulador;
+  for (var i = 0; i < selectorYears.length; i++){
+    selectorYears[i].innerHTML= acumulador;
+  }
 
-
-// CON LOS AÑOS DE 2017 A 1950
-
-
-// var selectorYears = document.querySelector ('.selector-year-to');
-// var acumulador= '';
-// var currentYear= 2017;
-// var firstYear= 1950;
-//
-// selectorYears.innerHTML + selectorYears.innerHTML + chooseYearsTo(2017,1950);
-//
-// function chooseYearsTo(currentYear,firstYear){
-//
-//   for (var i= currentYear; i >= firstYear; i++) {
-//     acumulador= acumulador + '<option>'+ i + '</option>'
-//   }
-//     return acumulador;
-// }
-//
-// selectorYears.innerHTML= acumulador;
-
-//EJEMPLO ALEX
-
-// selectorYears.innerHTML + selectorYears.innerHTML + chooseYears(1950, 2017);
-
-// var content = '';
-// var currentYear=2017;
-// for (var i = 0; i < array.length; i++) {
-// array[i]
-// }
-//
-// content += '<option>' + year + '</option>'
-// pruebaElem.innerHTML = content;
-//
-// --------------------------------------------------------------------------------
-//
-// function addYears(initYear, currentYear) {
-//   var acumulador= ''
-//   for (var i= initYear; i <= currentYear; i++) {
-//     acumulador= acumulador + '<option>'+ i + '</option>'
-//   }
-//   return acumulador;
-// }
-//
-// var selectorYears = document.querySelector ('#selectorYears');
-//
-// selectorYears.innerHTML + selectorYears.innerHTML + addYears(1950, 2017);
+  
