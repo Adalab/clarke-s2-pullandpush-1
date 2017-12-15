88bd6b7d7d0a7cf608df7fed1303aee9005882eb
Adalaber <mvtercero85@gmail.com>
2017-12-15 11:31:07 +0100

    Add calendar js to job and studies and add main text

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ personalData.onchange = function(event) {
 };
 
 
+
 // EXPERIENCE
 var hijoDelMal = document.querySelector('#satan');
 var jobExperience = document.querySelector('.job-experience');
@@ -33,26 +34,25 @@ jobExperience.onchange = function(event) {
     var aReJob = reviewJobs[i].querySelector('.js-company');
     aReJob.innerHTML = aJob.value + ' |';
 
-    var aJob = allJobs[i].querySelectorAll('.selector-month');
-    var aReJob = reviewJobs[i].querySelector('.js-jobdata');
-    aReJob.innerHTML = aJob.value;
-
-
-    // var allMonth= document.querySelectorAll('.selector-month');
-    // var reviewCalendar= document.querySelectorAll('.preview-job-set');
-    // for (var i = 0; i < allMonth.length; i++) {
-    //   var allSelectMonth= allMonth[i].querySelector('.js-jobdata');
-    //   allSelectMonth.innerHTML=
-    // }
-
-
+                // // var aJob = allJobs[i].querySelector('.calendar');
+                // var calendar = document.querySelector('.calendar');
+                // calendar.onchange = function(event) {
+                // var selectCalendarTo = document.querySelectorAll('.js-jobdata');
+                // var reviewCalendar = document.querySelectorAll('.preview-job-calendar');
+                // for (var i = 0; i < selectCalendarTo.length; i++) {
+                //   var aMonth= selectCalendarTo[i].querySelector('.selector-month');
+                //   var aReMonth= reviewCalendar[i].querySelector('js-jobdata');
+                //   aReMonth.innerHTML= aMonth.value + ' |';
+                // }
 
 
     var aJob = allJobs[i].querySelector('.description-into');
     var aReJob = reviewJobs[i].querySelector('.js-explanation');
     aReJob.innerHTML = aJob.value;
-  }
-};
+  // }
+}
+}
+
 
 //mover arriba
 var upButtonList = document.querySelectorAll('.up');
@@ -97,6 +97,89 @@ for (var i = 0; i < deleteButton.length; i++) {
   }
 }
 
+
+// CALENDARIO
+//MESES
+
+var monthOption= ['','enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+var acumuladorMonths= '';
+
+for (var i= 0; i <= monthOption.length; i++) {
+acumuladorMonths= acumuladorMonths + '<option>'+ monthOption[i] + '</option>';
+  }
+
+var selectorMonth= document.querySelectorAll('#selector-month');
+for (var i = 0; i < selectorMonth.length; i++){
+  selectorMonth[i].innerHTML= acumuladorMonths;
+}
+
+
+//AÑOS
+
+var selectorYears = document.querySelectorAll('#selector-year');
+var acumuladorYears= '';
+var currentYear= 1950;
+var firstYear= 2017;
+
+
+for (var i= firstYear; i >= currentYear; i--) {
+  acumuladorYears= acumuladorYears + '<option>'+ i + '</option>'
+}
+
+  for (var i = 0; i < selectorYears.length; i++){
+    selectorYears[i].innerHTML= acumuladorYears;
+  }
+
+//CALENDARIO JOB A PDF
+
+var selectCalendarFrom = document.querySelector('.calendar-job-from');
+selectCalendarFrom.addEventListener('change', calendarOptionsFrom);
+
+function calendarOptionsFrom() {
+  var monthFrom = document.querySelector('.month-from-job').value;
+  var yearFrom = document.querySelector('.year-from-job').value;
+  document.querySelector('.js-jobdata-from').innerHTML= ' ' + monthFrom + ' ' + yearFrom + '-';
+
+  }
+
+var selectCalendarTo = document.querySelector('.calendar-job-to');
+selectCalendarTo.addEventListener('change', calendarOptionsTo);
+
+function calendarOptionsTo() {
+  var monthTo = document.querySelector('.month-to-job').value;
+  var yearTo = document.querySelector('.year-to-job').value;
+
+  document.querySelector('.js-jobdata-to').innerHTML=' ' + monthTo + ' ' + yearTo;
+}
+
+var selectCalendarStudiesFrom = document.querySelector('.calendar-studies-from');
+selectCalendarStudiesFrom.addEventListener('change', calendarStudiesOptionsFrom);
+
+function calendarStudiesOptionsFrom() {
+  var monthStudiesFrom = document.querySelector('.month-from-studies').value;
+  var yearStudiesFrom = document.querySelector('.year-from-studies').value;
+  document.querySelector('.js-educationdate-from').innerHTML= ' ' + monthStudiesFrom + ' ' + yearStudiesFrom;
+
+  }
+
+  var selectCalendarStudiesTo = document.querySelector('.calendar-studies-to');
+  selectCalendarStudiesTo.addEventListener('change', calendarStudiesOptionsTo);
+
+  function calendarStudiesOptionsTo() {
+    var monthStudiesTo = document.querySelector('.month-to-studies').value;
+    var yearStudiesTo = document.querySelector('.year-to-studies').value;
+
+    document.querySelector('.js-educationdate-to').innerHTML=' ' + monthStudiesTo + ' ' + yearStudiesTo;
+  }
+
+
+
+
+
+
+
+//COLAPSABLES
+
 function toggleInput(target){
   if (target.classList.contains('hidden'))
   target.classList.remove('hidden');
@@ -129,7 +212,7 @@ function showFieldset(event) {
   tab[idFieldset].classList.toggle('hidden');
 }
 
-// var editButton = document.querySelectorAll('.edit-button');
+var editButton = document.querySelectorAll('.edit-button');
 
 
 for (var i = 0; i < fieldset.length; i++) {
@@ -152,40 +235,6 @@ window.print();
 // y esa variable que habíamos generado que parecía que
 document.body.innerHTML = pdf;
 
-}
+};
+
 printButton.addEventListener('click', printCv);
-
-/*JAVASCRIPT MARÍA*/
-
-
-//MESES
-var monthOption= ['','enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
-var acumuladorMonths= '';
-
-for (var i= 0; i <= monthOption.length; i++) {
-acumuladorMonths= acumuladorMonths + '<option>'+ monthOption[i] + '</option>';
-  }
-
-var selectorMonth= document.querySelectorAll('.selector-month');
-for (var i = 0; i < selectorMonth.length; i++){
-  selectorMonth[i].innerHTML= acumuladorMonths;
-}
-
-
-//AÑOS
-
-var selectorYears = document.querySelectorAll('.selector-year');
-var acumuladorYears= '';
-var currentYear= 1950;
-var firstYear= 2017;
-
-
-for (var i= firstYear; i >= currentYear; i--) {
-  acumuladorYears= acumuladorYears + '<option>'+ i + '</option>'
-}
-
-  for (var i = 0; i < selectorYears.length; i++){
-    selectorYears[i].innerHTML= acumuladorYears;
-  }
-
-  // //Cuando el usuario elige el mes en la lista de meses, ese mes tiene que incluirse en el PDF
